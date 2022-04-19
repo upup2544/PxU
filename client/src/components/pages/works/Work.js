9b920ce702539994ae80cbc032d377bab5640a97
upup2544 <upup2544@hotmail.com>
2022-04-19 10:54:15 +0700

    update

--- client/src/components/pages/works/Work.js
+++ client/src/components/pages/works/Work.js
@@ -3,11 +3,7 @@ import Axios from 'axios'
 import Modal from "../../../Modal/Modal";
 import Creatework from './Creatework'
 import moment from "moment";
-<<<<<<< HEAD
-import Createquotation from "../quotation/Createquotation";
-=======
 import CreateQuo from './createQuotation/CreateQuotation';
->>>>>>> 1e51ff56ac7430305436847bccb164b1d5ce1f80
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './work.css'
 
@@ -17,14 +13,10 @@ const Work = () => {
     const [count, setcount] = useState([]);
     const [work, setWork] = useState([]);
     const [show, setShow] = useState(false);
-<<<<<<< HEAD
-    const [createquotation, setcreatequotation] = useState(false);
-=======
     const [CreateDOCS, setCreateDOCS] = useState(false);
     const [CreatePDF, setCreatePDF] = useState(false);
     const [CreateQuotation, setCreateQuotation] = useState(false);
 
->>>>>>> 1e51ff56ac7430305436847bccb164b1d5ce1f80
     useEffect(() => {
         Axios.get(`http://localhost:8000/works`, {
         }).then((response) => {
@@ -38,75 +30,72 @@ const Work = () => {
                 <div className='banner'>
                     <h1> Works </h1>
                 </div>
-
                 <div className='navbar'>
                     <div className="navbar-left">
-
                     </div>
-
                     <div className="navbar-right">
-
                         <form className="search" onsubmit="event.preventDefault();" role="search">
                             <label for="search">Search for stuff</label>
                             <input id="search" type="search" placeholder="Search..." autofocus required />
                             <button type="submit">Go</button>
                         </form>
-
                         <button className="Createbutton" onClick={() => setShow(true)}>Create</button>
                         <Modal title="My Modal" onClose={() => setShow(false)} show={show}>
                             <Creatework />
                         </Modal>
                     </div>
                 </div>
-            </div>
-            <div className="body">
-                <table class="table table-hover table-striped">
-                    <thead>
-                        <tr>
-                            <th scope="col">#ID</th>
-                            <th scope="col">Title</th>
-                            <th scope="col">Date</th>
-                            <th scope="col">Due Date</th>
-                            <th> </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    {work.map((val, key) => {
-                        return (
-                            <tr className="itemsRow" >
-                                <td>{val.workID}</td>
-                                <td>{val.workName}</td>
-                                <td>{moment(val.startDate).format("DD-MM-YYYY")}</td>
-                                <td>{moment(val.endDate).format("DD-MM-YYYY")}</td>
-                                <td>
-                                    <td className="menubar">
-                                        {/* <td> <button class="bi bi-filetype-pdf" onClick={(e) => {return <ExportPdfComponent/>} }></button>  </td> */}
-                                        <td> <a class="bi bi-filetype-pdf" style={{ fontSize: 24 }}  ></a> </td>
-                                        <td> <a class="bi bi-file-earmark-word" style={{ fontSize: 24 }}  > </a> </td>
-                                        <td><a class="bi bi-send" style={{ fontSize: 24 }} onClick={() => {
-                                            setcreatequotation(true);
-                                        }} ></a>
-                                        <Modal title="ใบเสร็จ" onClose={() => setcreatequotation(false)} show={createquotation}>
-                                                <Createquotation data={val.workID} />
-                                            </Modal>
-                                            </td>
-                                        <td> <div className="editButt"> <a>Edit</a> </div></td>
-                                        <td> <a class="bi bi-trash" style={{ fontSize: 24 }} ></a> </td>
-                                    </td>
-                                </td>
+                <div className="body">
+                    <table class="table table-hover table-striped">
+                        <thead>
+                            <tr>
+                                <th scope="col">#ID</th>
+                                <th scope="col">Title</th>
+                                <th scope="col">Date</th>
+                                <th scope="col">Due Date</th>
+                                <th> </th>
                             </tr>
+                        </thead>
+                        <tbody>
+                            {work.map((val, key) => {
+                                return (
+                                    <tr className="itemsRow" >
+                                        <td>{val.workID}</td>
+                                        <td>{val.workName}</td>
+                                        <td>{moment(val.startDate).format("DD-MM-YYYY")}</td>
+                                        <td>{moment(val.endDate).format("DD-MM-YYYY")}</td>
+                                        <td>
+                                            <td className="menubar">
+                                                {/* <td> <button class="bi bi-filetype-pdf" onClick={(e) => {return <ExportPdfComponent/>} }></button>  </td> */}
+                                                <td><a class="bi bi-filetype-pdf" style={{ fontSize: 24 }}>
+                                                </a></td>
+                                                <td><a class="bi bi-file-earmark-word" style={{ fontSize: 24 }}  > </a> </td>
+                                                <td>
+                                                    <a class="bi bi-send" style={{ fontSize: 24 }} onClick={() => {
+                                                        console.log("I Clicked it ");
+                                                        setCreateQuotation(true);
+                                                        setcount(val.workID);
+                                                    }}>
+                                                        <Modal title="Create Quotation" onClose={() => setCreateQuotation(false)} show={CreateQuotation}>
+                                                            <WorkAuthContext.Provider value={count}>
+                                                                <CreateQuo />
+                                                            </WorkAuthContext.Provider>
+                                                        </Modal>
+                                                    </a></td>
+                                                <td> <div className="editButt"> <a>Edit</a> </div></td>
+                                                <td> <a class="bi bi-trash" style={{ fontSize: 24 }} ></a> </td>
+                                            </td>
+                                        </td>
+                                    </tr>
                                 );
                             })}
                         </tbody>
-
-
-
                     </table>
                 </div>
-
+                <row>
+                </row>
             </div>
-
         </div>)
 };
-export  {WorkAuthContext};
+export { WorkAuthContext };
 export default Work;
